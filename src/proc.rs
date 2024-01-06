5fed5f698ad63fe444e1345fc0a7222ab8443c2e
Xqhare <xqhare@web.de>
2024-01-06 22:21:40 +0100

    removed impl of proc

--- src/proc.rs
+++ src/proc.rs
@@ -1,18 +1,11 @@
 use sysinfo::Pid;
 
-
-#[derive(Debug, Clone, Copy)]
-struct Proc {
-    name: &str,
-    pid: Pid,
-    parent_name: &str,
-    parent_pid: Pid,
-    cpu_usage_per: f32,
-    date: String,
-}
-
-impl Proc {
-    fn new(name: &str, pid: Pid, parent_name: &str, parent_pid: Pid, cpu_usage_per: f32, date: String) -> Self {
-        Proc { name, pid, parent_name, parent_pid, cpu_usage_per, date }
-    }
+#[derive(Debug, Clone)]
+pub struct Proc {
+    pub name: String,
+    pub pid: Pid,
+    pub parent_name: String,
+    pub parent_pid: Pid,
+    pub cpu_usage_per: f32,
+    pub date: String,
 }
